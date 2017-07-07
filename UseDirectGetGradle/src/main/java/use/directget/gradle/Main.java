553b82658ca12df82ac2a62069487ed330b17242
NawaMan <nawa@dssbsoft.com>
2017-07-07 02:34:32 -0600

    Add a Run.

--- UseDirectGetGradle/src/main/java/use/directget/gradle/Main.java
+++ UseDirectGetGradle/src/main/java/use/directget/gradle/Main.java
@@ -2,6 +2,7 @@ package use.directget.gradle;
 
 import directget.get.Get;
 import directget.get.Ref;
+import directget.get.Run;
 
 public class Main {
     
@@ -9,6 +10,12 @@ public class Main {
     
     public static void main(String[] args) {
         System.out.println(Get.the(greetingMsg));
+        
+        Run
+        .with(greetingMsg.providedWith("Hey!"))
+        .run(()->{
+            System.out.println(Get.the(greetingMsg));
+        });
     }
     
 }
